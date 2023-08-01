388e513fac0f5ca80db5d218554381f5de81a663
tames_jam <jamescktam09@hotmail.co.uk>
2023-08-01 11:23:50 +0100

    Completed SpringController.java, integrating controller front-end to services back-end

--- src/main/java/com/sky/Week3Spring/rest/SpringController.java
+++ src/main/java/com/sky/Week3Spring/rest/SpringController.java
@@ -2,6 +2,7 @@ package com.sky.Week3Spring.rest;
 
 import com.sky.Week3Spring.domain.Person;
 import com.sky.Week3Spring.services.SpringService;
+import com.sky.Week3Spring.services.SpringServiceList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
@@ -11,55 +12,32 @@ import org.springframework.web.bind.annotation.*;
 import java.util.*;
 
 @RestController
+@RequestMapping("/person")
 public class SpringController {
 
-    private List<Person> persons = new ArrayList<>();
+    private SpringServiceList service;
 
     public SpringController() {
     }
 
-    @GetMapping("/hello")
-    public String hello() {
-        return "Hello World!";
-    }
-
     @PostMapping("/createPerson")
     public HttpEntity<Person> createPerson(@RequestBody Person person) {
-        System.out.println("Received a Person object: " + person);
-        persons.add(person);
-        Person created = this.persons.get(this.persons.size() - 1);
-        return new ResponseEntity<>(created, HttpStatus.CREATED);
+        return new ResponseEntity<>(service.createPerson(person), HttpStatus.CREATED);
     }
 
     @PostMapping("/createPeople")
     public HttpEntity<List<Person>> createPeople(@RequestBody List<Person> persons) {
-        System.out.println("Received a list of Persons objects: " + persons);
-        this.persons.addAll(persons);
-        List<Person> created = new ArrayList<>();
-
-        for (int i = 0; i < persons.size(); i++) {
-            created.add(this.persons.get(i));
-        }
-
-        return new ResponseEntity<>(created, HttpStatus.CREATED);
+        return new ResponseEntity<>(service.createPeople(persons), HttpStatus.CREATED);
     }
 
     @GetMapping("/getPeople")
     public HttpEntity<List<Person>> getPeople() {
-        System.out.println("Getting multiple Persons object: ");
-        for (Person p : this.persons) {
-            System.out.println(p);
-        }
-        return new ResponseEntity<>(this.persons, HttpStatus.OK);
+        return new ResponseEntity<>(this.service.getPeople(), HttpStatus.OK);
     }
-
-
 
     @GetMapping("/getPerson/{id}") // Specific Person with ID given by its unique index
     public HttpEntity<Person> getPerson(@PathVariable Integer id) {
-        System.out.println("Getting singular Person object: ");
-        Person person = this.persons.get(id);
-        return new ResponseEntity<>(person, HttpStatus.OK);
+        return new ResponseEntity<>(this.service.getPerson(id), HttpStatus.OK);
     }
 
     @PatchMapping("/update/{id}")
@@ -67,17 +45,11 @@ public class SpringController {
                                      @RequestParam String firstName,
                                      @RequestParam String lastName,
                                      @RequestParam (value = "age", required = true) Integer age) {
-        System.out.println("UPDATE for Person ID: " + id + " first name: " + firstName + " last name: " + lastName + " age: " + age);
-        this.persons.get(id).setFirstName(firstName);
-        this.persons.get(id).setLastName(lastName);
-        this.persons.get(id).setAge(age);
-        return new ResponseEntity<>(this.persons.get(id), HttpStatus.OK);
+        return new ResponseEntity<>(this.service.update(id, firstName, lastName, age), HttpStatus.OK);
     }
 
     @DeleteMapping("/delete/{id}")
-    public HttpEntity<List<Person>> delete(@PathVariable Integer id) {
-        System.out.println("DELETE for Person ID: " + id);
-        this.persons.remove(id.intValue());
-        return new ResponseEntity<>(this.persons, HttpStatus.OK);
+    public HttpEntity<Boolean> delete(@PathVariable Integer id) {
+        return new ResponseEntity<>(this.service.delete(id), HttpStatus.OK);
     }
 }
